253f9b45a93d83b97e5b418d48bda24751104767
Chafik-Rd <78787036+Chafik-Rd@users.noreply.github.com>
2025-10-08 02:53:25 +0700

    edit index.ts

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -4,5 +4,5 @@ import { neon } from "@neondatabase/serverless";
 import { env } from "~/env";
 import * as schema from "./schema";
 
-const sql = neon(env.POSTGRES_URL! as string);
+const sql = neon(env.POSTGRES_URL);
 export const db = drizzle(sql, { schema });

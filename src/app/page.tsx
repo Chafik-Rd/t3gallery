93795f3cb7a038f8fcfcd8a115896ef5a61142a9
Chafik-Rd <78787036+Chafik-Rd@users.noreply.github.com>
2025-10-09 11:44:49 +0700

    Use auth for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,36 @@
 import { db } from "~/server/db";
 import { images } from "~/server/db/schema";
 import { desc } from "drizzle-orm";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const imagesList: (typeof images.$inferSelect)[] = await db
     .select()
     .from(images)
     .orderBy(desc(images.id));
+  return (
+    <div className="flex flex-wrap gap-4">
+      {[...imagesList, ...imagesList, ...imagesList].map((image, index) => (
+        <div key={image.id + "-" + index} className="flex w-48 flex-col">
+          <img src={image.url} alt="image" />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
 
+export default async function HomePage() {
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {[...imagesList, ...imagesList,...imagesList].map((image, index) => (
-          <div key={image.id + "-" + index} className="flex w-48 flex-col">
-            <img src={image.url} alt="image" />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
-      Hello (gallery in progress)
+      <SignedOut>
+        <div className="h-full w-full text-2xl text-center">Please sign in above</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
